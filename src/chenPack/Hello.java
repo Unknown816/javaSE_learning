7083f210b759e47388f8da035ca0d0937c17a28c
chen <c19901008@qq.com>
2020-10-09 21:07:25 +0800

    第二次提交

--- src/chenPack/Hello.java
+++ src/chenPack/Hello.java
@@ -6,9 +6,6 @@ public class Hello {
         int j = 167776512;//00001010 00000000 00010001 00000000
         int num = i & j;  //00001010 00000000 00010001 00000000
         int ans=0x11C;
-        if(1.0==1){
-            System.out.println("int and double are equal!");
-        }
         System.out.println(ans);
         System.out.println(Integer.toHexString(num));
         System.out.println(Integer.parseInt("0101",2));
