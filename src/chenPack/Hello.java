770acbdffb9bde9139f227b895a90f921e725643
chen <c19901008@qq.com>
2020-10-09 20:43:24 +0800

    第一次提交javaSE练习

---
+++ src/chenPack/Hello.java
@@ -0,0 +1,16 @@
+package chenPack;
+
+public class Hello {
+    public static void main(String[] args) {
+        int i = 167776589;//00001010 00000000 00010001 01001101
+        int j = 167776512;//00001010 00000000 00010001 00000000
+        int num = i & j;  //00001010 00000000 00010001 00000000
+        int ans=0x11C;
+        if(1.0==1){
+            System.out.println("int and double are equal!");
+        }
+        System.out.println(ans);
+        System.out.println(Integer.toHexString(num));
+        System.out.println(Integer.parseInt("0101",2));
+    }
+}
