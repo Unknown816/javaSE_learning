57e30378d34a5a83923389b597e998a8a1ffc755
chen <c19901008@qq.com>
2020-10-10 23:15:30 +0800

    if-else if-else BMI计算

--- src/chenPack/Hello.java
+++ src/chenPack/Hello.java
@@ -4,8 +4,20 @@ import java.util.Scanner;
 public class Hello {
     public static void main(String[] args) {
         Scanner scanner= new Scanner(System.in);
-        int pastSC=scanner.nextInt();
-        int nowSC=scanner.nextInt();
-        System.out.printf("%.2f%%",100.0*(nowSC-pastSC)/pastSC);
+        double weight=scanner.nextDouble();
+        double height=scanner.nextDouble();
+        double bmi=weight/(height*height);
+        System.out.printf("BMI : %.2f\n",bmi);
+        if(bmi>32){
+            System.out.println("非常肥胖");
+        }else if(bmi>=28){
+            System.out.println("肥胖");
+        }else if(bmi>=25){
+            System.out.println("过重");
+        }else if(bmi>=18.5){
+            System.out.println("正常");
+        }else{
+            System.out.println("过轻");
+        }
     }
 }
