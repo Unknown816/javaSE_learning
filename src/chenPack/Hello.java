57dcbaa9d3d72d14f6731ae3b9d56f71b6143617
chen <c19901008@qq.com>
2020-10-10 21:55:18 +0800

    输入输出练习1

--- src/chenPack/Hello.java
+++ src/chenPack/Hello.java
@@ -1,13 +1,11 @@
 package chenPack;
+import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        int i = 167776589;//00001010 00000000 00010001 01001101
-        int j = 167776512;//00001010 00000000 00010001 00000000
-        int num = i & j;  //00001010 00000000 00010001 00000000
-        int ans=0x11C;
-        System.out.println(ans);
-        System.out.println(Integer.toHexString(num));
-        System.out.println(Integer.parseInt("0101",2));
+        Scanner scanner= new Scanner(System.in);
+        int pastSC=scanner.nextInt();
+        int nowSC=scanner.nextInt();
+        System.out.printf("%.2f%%",100.0*(nowSC-pastSC)/pastSC);
     }
 }
